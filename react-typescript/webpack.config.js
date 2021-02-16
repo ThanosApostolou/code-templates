b552ec4f70e16a952c91583d114a5fb0142eb78f
ThanosApostolou <thanosapostolou@outlook.com>
2021-02-16 12:21:31 +0200

    minor change

--- react-typescript/webpack.config.js
+++ react-typescript/webpack.config.js
@@ -25,6 +25,9 @@ module.exports = {
         {
             test: /\.(png|svg|jpg|jpeg|gif)$/i,
             type: "asset/resource",
+            generator: {
+                filename: 'assets/[name][ext][query]'
+            }
         },
 
         // css-loader to bundle all the css files into one file and style-loader to add all the styles  inside the style tag of the document
