26729463350fb35437098e70e569a938a736bbc1
ThanosApostolou <thanosapostolou@outlook.com>
2021-02-18 10:22:52 +0200

    update webpack config

--- node-typescript-template/webpack.config.js
+++ node-typescript-template/webpack.config.js
@@ -2,46 +2,49 @@ const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const ESLintPlugin = require("eslint-webpack-plugin");
 
-module.exports = {
-    entry: "./src/main.ts",
-    target: 'node',
-    output: {
-        path: path.join(__dirname, "/build"),
-        filename: "bundle.js"
-    },
-    resolve: {
-        extensions: [".ts", ".js"]
-    },
+module.exports = (env, argv) => {    
+    const isProd = argv.mode === 'production';
+    return {
+        entry: "./src/main.ts",
+        target: 'node',
+        output: {
+            path: path.join(__dirname, "/build"),
+            filename: "bundle.js"
+        },
+        resolve: {
+            extensions: [".ts", ".js"]
+        },
 
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: {
-                    loader: "ts-loader"
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: {
+                        loader: "ts-loader"
+                    },
+                    exclude: [/node_modules/, /build/]
                 },
-                exclude: [/node_modules/, /build/]
-            },
-            {
-                test: /\.js$/,
-                exclude: [/node_modules/, /build/]
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-                generator: {
-                    filename: 'assets/[name][ext][query]'
+                {
+                    test: /\.js$/,
+                    exclude: [/node_modules/, /build/]
                 },
-                exclude: [/node_modules/, /build/]
-            }
+                {
+                    test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                    type: "asset/resource",
+                    generator: {
+                        filename: 'assets/[name][ext][query]'
+                    },
+                    exclude: [/node_modules/, /build/]
+                }
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new ESLintPlugin({
+                context: "src/",
+                extensions: ["js", "ts"],
+                fix: true
+            })
         ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new ESLintPlugin({
-            context: "src/",
-            extensions: ["js", "ts"],
-            fix: true
-        })
-    ]
+    };
 };
